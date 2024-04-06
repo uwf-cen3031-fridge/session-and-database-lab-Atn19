2272c4f9d42387ef4ff116cbadcd61c3d8ccf6d5
Atn19 <atn19@students.uwf.edu>
2024-04-05 21:35:50 -0500

    Done now

--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { pino } from 'pino';
+import { pino } from "pino";
 import { UserService } from "../services/user.service";
 
 export class AppController {
@@ -11,26 +11,13 @@ export class AppController {
   }
 
   private initializeRouter() {
-
-    this.router.get("/login", (req: Request, res: Response) => {
+    this.router.get("/login", function (req, res) {
       res.render("login");
     });
 
-    // Handle login form submissions
-    this.router.post("/login", async (req: any, res) => {
-      const user = await this.userService.authenticateUser(req.body.username, req.body.password);
-      if (user) {
-        req.session.user = user;
-        res.redirect("/");
-      } else {
-        res.status(401).send("Invalid username or password");
-      }
-    });
-
-    this.router.get("/logout", (req: Request, res: Response) => {
-      req.session.destroy(() => {
-        res.redirect("/");
-      })
+    this.router.get("/logout", function (req:any, res) {
+      delete req.session.user;
+      res.render("login");
     });
 
     this.router.get("/signup", function (req, res) {
@@ -43,27 +30,41 @@ export class AppController {
       res.redirect("/");
     });
 
-    // PROTECT THE HOMEPAGE
+    // Handle login form submissions
+    this.router.post("/processLogin", async (req: any, res) => {
+      const user = await this.userService.authenticateUser(req.body.username, req.body.password);
+      if (user) {
+        req.session.user = user;
+        res.redirect("/");
+      } else {
+        res.status(401).send("Invalid username or password");
+      }
+    });
 
-    this.router.use((req: any, res: Response, next) => {
+    // Enforce security
+    this.router.use((req: any, res, next) => {
+      // If the user is set in the session,
+      // pass them on
       if (req.session.user) {
         next();
+
+        // Otherwise, send them to the login page
       } else {
-        res.redirect("/login");
+        res.render("login", {
+          error: "You need to log in first",
+        });
       }
     });
 
     // Serve the home page
     this.router.get("/", (req: any, res: Response) => {
       try {
-        // Render the "home" template as HTML
         res.render("home", {
-          user: req.session.user
+          user: req.session.user,
         });
       } catch (err) {
         this.log.error(err);
       }
     });
-
   }
 }
